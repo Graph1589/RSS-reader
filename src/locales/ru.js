848ab5b53f716aa93d1446ba63047235b5b25941
graph1589 <graph1589@gmail.com>
2021-10-02 11:56:30 +0300

    locales reworked, ru added

---
+++ src/locales/ru.js
@@ -0,0 +1,25 @@
+export default {
+  translation: {
+    errors: {
+      input: {
+        url: 'Ссылка должна быть валидным URL',
+        notOneOf: 'RSS уже существует',
+        notEmpty: 'Не должно быть пустым',
+      },
+      feed: {
+        wrongData: 'Ресурс не содержит валидный RSS',
+        network: 'Ошибка сети',
+      },
+    },
+    messages: {
+      added: 'RSS успешно загружен',
+    },
+    output: {
+      feeds: 'Фиды',
+      posts: 'Посты',
+      prewiew: 'Просмотр',
+      full: 'Читать полностью',
+      close: 'Закрыть',
+    }
+  },
+};
