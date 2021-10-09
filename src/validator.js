cf243fe65c15aaa4ff506706a928e5c8ce57cc57
graph1589 <graph1589@gmail.com>
2021-10-09 14:15:14 +0300

    trying to pass autotests

--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,12 @@
 import * as yup from 'yup';
 
-export default (url, list) => yup.string()
+/* export default (url, list) => yup.string()
   .url()
   .notOneOf(list)
   .required()
   .validate(url);
+*/
+export default (url, list) => {
+  const schema = yup.string().notOneOf(list).required();
+  return schema.validate(url);
+}
